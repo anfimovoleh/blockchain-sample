752c2b15c47e9cadc12450e2d64d56e0ac3a7bcf
Anfimov <uanavaro@gmail.com>
2019-10-20 08:03:27 +0300

    add documentation, examples for the executable file, add spf13/cobra

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,29 @@ import (
 	"github.com/anfimovoleh/blockchain-sample"
 	"github.com/anfimovoleh/blockchain-sample/conf"
 	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	cfg := conf.New()
 
-	app := app.New(cfg)
+	root := cobra.Command{}
+	start := &cobra.Command{
+		Use:     "start",
+		Short:   "start Blockchain Node",
+		Long:    "Start the Blockchain Node with provided REST'ful API and empty ledger",
+		Example: "./node start",
+		Run: func(cmd *cobra.Command, args []string) {
+			app := app.New(cfg)
 
-	if err := app.Start(); err != nil{
-		panic(errors.Wrap(err, "failed to start app"))
+			if err := app.Start(); err != nil {
+				panic(errors.Wrap(err, "failed to start app"))
+			}
+		},
+	}
+
+	root.AddCommand(start)
+	if err := root.Execute(); err != nil {
+		panic(err)
 	}
 }
